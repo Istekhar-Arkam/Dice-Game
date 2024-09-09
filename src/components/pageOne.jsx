849226f102756e9720934e9beb0f0c9272a7d310
istekhar <amreennaaz888@gmail.com>
2024-09-09 18:33:50 +0530

    created front Page

--- src/components/pageOne.jsx
+++ src/components/pageOne.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import dices1 from "../assets/image/dices1.png"
+import React from "react";
+import dices1 from "../assets/image/dices1.png";
+import Button from "../globalComponents/button";
 const PageOne = () => {
   return (
     <>
-    {/* container */}
-    <div className="flex justify-center m-2 ">
-    <div className="border-4 border-red-600 max-w-6xl px-3 py-3">
-     <img src={dices1} alt="dice-image" /></div>
-     <div>
-      <h1>Istekhar Arkam</h1>
-     </div>
-    </div>
+      {/* container */}
+      <div className="m-2 my-15 sm:flex sm:my-20">
+        <div className="max-w-6xl px-3 py-3">
+          <img src={dices1} alt="dice-image" />
+        </div>
+        <div className="flex flex-col items-center sm:my-32 lg:mx-20 sm:whitespace-nowrap">
+          <h1 className="uppercase font-bold text-5xl py-10">Dice Game</h1>
+          <Button />
+        </div>
+      </div>
     </>
-    
-  )
-}
+  );
+};
 
-export default PageOne
+export default PageOne;
---
+++ src/globalComponents/button.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <>
+      <button
+        type="button"
+        class="rounded-md bg-black px-10 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+      >
+        {" "}
+        Play Now
+      </button>
+    </>
+  );
+};
+
+export default Button;
